0caa29e3a295e9a05fa4aad229e79f67b1c35c5a
komplex2103 <75624077+komplex2103@users.noreply.github.com>
2021-01-09 00:05:59 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,66 @@
+var bullet , wall, thickness;
+var speed , weight; 
+
+function setup() {
+  createCanvas(1800,400);
+
+
+  thickness = random(22 , 83);
+
+  bullet = createSprite(50, 200, 50, 10);
+ wall = createSprite(1200, 200 , thickness , 100);
+  
+
+  speed = random(223,321);
+  weight = random(30,52);
+
+
+  bullet.velocityX = speed; 
+
+}
+
+function draw() {
+  background("black"); 
+  
+  if(wall.x - bullet.x < (bullet.width+wall.width)/2)
+  {
+    bullet.velocityX = 0;
+    var deformation = 0.5 * weight * speed *speed/22509;
+    if (deformation > 180)
+    {
+      bullet.shapeColor = "white";
+    }
+    if (deformation < 180)
+    {
+      bullet.shapeColor = "white";
+    }
+    if (deformation < 100)
+    {
+      bullet.shapeColor = "white";
+    }
+  }
+  if (hascollided(bullet,wall))
+  {
+    bullet.velocityX = 0;
+     var damage = 0.5 * weight * speed * speed/(thickness * thickness * thickness);
+
+     if (damage > 10)
+     {
+       wall.shapeColor = "red";
+     }
+     if (damage < 10)
+     {
+       wall.shapeColor = "green";
+     }
+  }
+
+  drawSprites();
+}
+function hascollided (bullet,wall){
+  bulletRightEdge = bullet.x - bullet.width;
+  wallLeftEdge = wall.x;
+  if (bulletRightEdge >= wallLeftEdge){
+    return true
+  }
+  return false;
+}
